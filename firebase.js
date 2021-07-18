635aada66a0444ef653333a3247a8c9144e55312
ItsImpyy10 <pratik.don2011@gmail.com>
2021-07-18 18:59:24 -0400

    checkout page

---
+++ firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase'
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBuS0Z_clQA7yp_waFkMfJdx1MCDswjcr4",
+    authDomain: "clone2-3b8f2.firebaseapp.com",
+    projectId: "clone2-3b8f2",
+    storageBucket: "clone2-3b8f2.appspot.com",
+    messagingSenderId: "663873488868",
+    appId: "1:663873488868:web:ee03b0e73c158de13269f1",
+    measurementId: "G-EDQ4TZE1W3"
+  };
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,13 +5,22 @@ import {
   SearchIcon,
   ShoppingCartIcon,
 } from "@heroicons/react/outline";
+import { signIn, signOut, useSession } from "next-auth/client";
+import { useRouter } from "next/router";
+import { useSelector } from "react-redux";
+import { selectItems } from "../slices/basketSlice";
 
 function Header() {
+  const [session] = useSession();
+  const router = useRouter();
+  const items = useSelector(selectItems);
+
   return (
     <header>
       <div className="flex item-center bg-amazon_blue p-1 flex-grow py-2">
         <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
           <Image
+            onClick={() => router.push("/")}
             src="https://links.papareact.com/f90"
             width={150}
             height={40}
@@ -28,17 +37,23 @@ function Header() {
           <SearchIcon className="h-12 p-4 " />
         </div>
         <div className="text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap">
-          <div className=" link">
-            <p>Hello Impyy</p>
+          <div
+            onClick={!session ? signIn : signOut}
+            className="cursor-pointer link"
+          >
+            <p>{session ? `Hello, ${session.user.name}` : "Sign in"}</p>
             <p className="font-extrabold  md:text-small">Account & Lists</p>
           </div>
           <div className=" link">
             <p>Returns</p>
             <p className="font-extrabold  md:text-small">& Orders</p>
           </div>
-          <div className=" relative link flex items-center ">
+          <div
+            onClick={() => router.push("/checkout")}
+            className=" relative link flex items-center "
+          >
             <span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold">
-              0
+              {items.length}
             </span>
             <ShoppingCartIcon className="h-10" />
             <p className="hidden md:inline font-extrabold  md:text-sm mt-2">
@@ -55,12 +70,12 @@ function Header() {
         <p className="link">Prime Video</p>
         <p className="link">Amazon Business</p>
         <p className="link">Today's Deal</p>
-        <p className='link hidden lg:inline-flex'>Electronics</p>
-        <p className='link hidden lg:inline-flex'>Food & Grocery</p>
-        <p className='link hidden lg:inline-flex'>Prime</p>
-        <p className='link hidden lg:inline-flex'>Buy Again</p>
-        <p className='link hidden lg:inline-flex'>Shopper Toolkit</p>
-        <p className='link hidden lg:inline-flex'>Health & Personal Care</p>
+        <p className="link hidden lg:inline-flex">Electronics</p>
+        <p className="link hidden lg:inline-flex">Food & Grocery</p>
+        <p className="link hidden lg:inline-flex">Prime</p>
+        <p className="link hidden lg:inline-flex">Buy Again</p>
+        <p className="link hidden lg:inline-flex">Shopper Toolkit</p>
+        <p className="link hidden lg:inline-flex">Health & Personal Care</p>
       </div>
     </header>
   );
